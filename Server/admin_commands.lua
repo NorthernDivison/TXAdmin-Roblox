7f178855969557cc1341a9143d95e23526798f4a
NorthernDivison <bob783787@gmail.com>
2026-03-02 02:32:49 -0500

    Create admin_commands.lua

---
+++ Server/admin_commands.lua
@@ -0,0 +1,6 @@
+-- kickPlayer(player)
+-- Description: Kicks a player from the game if they are not an admin
+-- Usage: kickPlayer(game.Players["TargetPlayerName"])
+function kickPlayer(player)
+    -- code goes here
+end
